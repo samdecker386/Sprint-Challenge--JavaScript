b4d9d49c0ae5f00c7a92328e2388aa5577ce6197
Sam Decker <samdecker386@gmail.com>
2020-01-31 14:48:05 -0700

    Fixed double declaration and line typo

--- challenges/classes.js
+++ challenges/classes.js
@@ -1,6 +1,6 @@
 // 1. Copy and paste your prototype in here and refactor into class syntax.
 
-class CuboidMaker {
+class CuboidMaker2 {
 	constructor(attributes) {
 		this.length = attributes.length;
   	this.width = attributes.width;
@@ -16,19 +16,19 @@ class CuboidMaker {
 	}
 }
 
-const cuboid = new CuboidMaker({
+const cuboid2 = new CuboidMaker2({
   length: 4,
   width: 5,
   height: 5
 });
 
 // Test your volume and surfaceArea methods by uncommenting the logs below:
-console.log(cuboid.volume()); // 100
-console.log(cuboid.surfaceArea()); // 130
+console.log(cuboid2.volume()); // 100
+console.log(cuboid2.surfaceArea()); // 130
 
 // Stretch Task: Extend the base class CuboidMaker with a sub class called CubeMaker.  Find out the formulas for volume and surface area for cubes and create those methods using the dimension properties from CuboidMaker.  Test your work by logging out your volume and surface area.
 
-class CubeMaker extends CuboidMaker {
+class CubeMaker extends CuboidMaker2 {
     constructor(attributes) {
       super(attributes)
     }
